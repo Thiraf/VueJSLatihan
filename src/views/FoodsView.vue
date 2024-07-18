20bc6b3577c4cc31dd846fd197a870516fa04452
Ananda Hirzi Thirafi <119308548+Thiraf@users.noreply.github.com>
2024-07-18 10:00:52 +0700

    first commit

---
+++ src/views/FoodsView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <navbar />
+    <h2> Daftar Makanan</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  // Kurang tau fungsinya
+    name : "FoodsView"
+
+}
+</script>
+
+<style>
+
+</style>
